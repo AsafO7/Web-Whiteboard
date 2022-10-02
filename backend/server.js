3cc3a5cbdb5e57f2acd8e9285fb8c0cf20c5adaf
AsafO7 <ovasaf@gmail.com>
2022-10-02 14:53:22 +0300

    first commit

--- backend/server.js
+++ backend/server.js
@@ -44,4 +44,5 @@ else {
     app.get('/', (req, res) => res.send('Please set to production'));
 }
 
-app.listen(process.env.PORT || 5000, () => console.log("Listening on port 5000"));
+const port = process.env.PORT || 5000
+app.listen(port, () => console.log("Listening on port 5000"));
