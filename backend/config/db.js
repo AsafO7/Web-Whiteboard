04a6d049087d62d6654748c97037d5413752bced
AsafO7 <ovasaf@gmail.com>
2022-10-03 17:08:52 +0300

    first commit

--- backend/config/db.js
+++ backend/config/db.js
@@ -5,7 +5,7 @@ const connectDB = async () => {
     await mongoose.connect(process.env.MONGO_URI, () => console.log("Connected to database"))
   } catch (error) {
     console.log(error)
-    process.exit(1)
+    // process.exit(1)
   }
 }
 
