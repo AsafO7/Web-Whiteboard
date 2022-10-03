9c995dbffa797d419fc69775074767960027160e
AsafO7 <ovasaf@gmail.com>
2022-10-03 17:02:06 +0300

    first commit

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const dotenv = require('dotenv').config();
+const dotenv = require('dotenv').config({ path: 'ENV_FILENAME' });
 const cors = require('cors')
 const connectDB = require('./config/db');
 const { errorHandler } = require('./middleware/errorMiddleware');
