7128cfe1e8cd4586490a1c2ab6ef6b4b1a6bc63b
AsafO7 <ovasaf@gmail.com>
2023-02-07 09:23:31 +0200

    back to localhost

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
     cors: {
-        origin: "172.105.130.95:3000",
+        origin: "http://localhost:3000",
         methods: ["GET", "POST"],
     },
 })
--- frontend/src/app/apiCalls.tsx
+++ frontend/src/app/apiCalls.tsx
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 export const loginUser = async (user: { name: string | undefined, email: string | undefined, password: string | undefined }) => {
     try {
-        const response = await axios.post("172.105.130.95:5000/lobby", user)
+        const response = await axios.post("http://localhost:5000/lobby", user)
         return response.data
     }
     catch(err) {
@@ -13,7 +13,7 @@ export const loginUser = async (user: { name: string | undefined, email: string 
 // When user refreshes the page or closes the tab
 export const logoutUser = async (userInfo: { name: String, email: String, currentRoom: String }) => {    
     try {
-        const response = await axios.post("172.105.130.95:5000/", userInfo)
+        const response = await axios.post("http://localhost:5000/", userInfo)
         return response.data
     }
     catch(err) {
@@ -24,7 +24,7 @@ export const logoutUser = async (userInfo: { name: String, email: String, curren
 // When user leaves a room (go back button)
 export const updateUserRoom = async ( userInfo: { name: String, email: String, currentRoom: String }) => {
     try {
-        const response = await axios.put("172.105.130.95:5000/room", userInfo)
+        const response = await axios.put("http://localhost:5000/room", userInfo)
         return response.data
     }
     catch(err) {
@@ -34,7 +34,7 @@ export const updateUserRoom = async ( userInfo: { name: String, email: String, c
 
 export const getLobbyInfo = async (user: { name: String, email: String, currentRoom: String }) => {
     try {
-        const response = await axios.get("172.105.130.95:5000/lobby", { params: user })
+        const response = await axios.get("http://localhost:5000/lobby", { params: user })
         return response.data
     }
     catch(err) {
@@ -44,7 +44,7 @@ export const getLobbyInfo = async (user: { name: String, email: String, currentR
 
 export const createRoom = async (userName: String, roomName: string | undefined, roomId: String) => {
     try {
-        const response = await axios.post("172.105.130.95:5000/room", {userName, roomName, roomId})
+        const response = await axios.post("http://localhost:5000/room", {userName, roomName, roomId})
         return response.data
     }
     catch(err) {
@@ -55,7 +55,7 @@ export const createRoom = async (userName: String, roomName: string | undefined,
 export const getRoomInfo = async (roomId: String, username: String) => {
     const params = { roomId, username }
     try {
-        const response = await axios.get(`172.105.130.95:5000/room/:${roomId}`, { params: params })
+        const response = await axios.get(`http://localhost:5000/room/:${roomId}`, { params: params })
         return response.data
     }
     catch(err) {
